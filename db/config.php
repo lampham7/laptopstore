64ad3743c3467fbcf161c890f13ee263f42a03f0
sonerror <nguyenvansonawm@gmail.com>
2022-12-03 22:07:56 +0700

    data

---
+++ db/config.php
@@ -0,0 +1,13 @@
+<?php
+    const host = "localhost";//
+    const user = "root";//
+    const password = "";//
+    const database = "laptopstore";//
+    const baseUrl = "http://localhost/laptopstore/";//
+    const origin = "http://localhost";//
+    const allowedOrigins = [//
+        'http://localhost',
+        'http://localhost:3000',
+        'http://localhost/laptopstore/'
+    ];
+?>
