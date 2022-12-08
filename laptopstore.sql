dcf28392aba218886a36548c731b1ece6cf3bb70
Lam_Pham <77009602+lampham7@users.noreply.github.com>
2022-12-08 21:45:09 +0700

    Update laptopstore.sql

--- laptopstore.sql
+++ laptopstore.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 29, 2022 lúc 09:19 AM
--- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Thời gian đã tạo: Th11 29, 2022 lúc 09:19 AM
+-- Phiên bản máy phục vụ: 10.4.22-MariaDB
 -- Phiên bản PHP: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
